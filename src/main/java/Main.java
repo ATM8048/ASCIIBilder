6c49c4c613bc0ac71d71373d784bcef105de72ca
Martin Atanasov <martin.atanasov@lernende.bbw.ch>
2022-12-03 11:23:17 +0100

    Projekt hingefügt

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("((_))");
+        System.out.println("(O.o)");
+        System.out.println("'(\")(\")'");
+
+        //Knacknussaufgabe
+            System.out.println("(\\(\\");
+            System.out.println("(-.-)");
+            System.out.println("o_(\")(\")");
+        //mehrzahlige Hase:
+            System.out.println("(\\(\\\n"+
+                    "(-.-)\n"+
+                    "o_(\")(\")\n");
+            System.out.println("((_))\n"+
+                    "(O.o)\n"+
+                    "'(\")(\")'\n");
+    }
+}
